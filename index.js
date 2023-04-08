0ad7560b965e031f493f0624b8e24e63d6fccbf1
mudassarsindoli <129504852+mudassarsindoli@users.noreply.github.com>
2023-04-08 22:16:46 +0530

    add initial files of noDice project

---
+++ index.js
@@ -0,0 +1,31 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1   //random number is generated ranging from 1 to 6
+
+let randomNumber1Source = randomNumber1 + ".png"; //Adds .png extension to the random number
+
+let image1 = document.querySelectorAll("img")[0]; //selects the first image in red div
+
+image1.setAttribute("src", randomNumber1Source) // changes the source of the img based on number generated
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1   //random number is generated ranging from 1 to 6
+
+let randomNumber2Source = randomNumber2 + ".png";    //Adds .png extension to the random number
+
+let image2 = document.querySelectorAll("img")[1];  //selects the first image in red div
+
+image2.setAttribute("src", randomNumber2Source) // changes the source of the img based on number generated
+
+let randomNumber3 = Math.floor(Math.random() * 6) + 1   //random number is generated ranging from 1 to 6
+
+let randomNumber3Source = randomNumber3 + ".png";    //Adds .png extension to the random number
+
+let image3 = document.querySelectorAll("img")[2];//selects the first image in red div
+
+image3.setAttribute("src", randomNumber3Source) // changes the source of the img based on number generated
+
+let randomNumber4 = Math.floor(Math.random() * 6) + 1   //random number is generated ranging from 1 to 6
+
+let randomNumber4Source = randomNumber4 + ".png";    //Adds .png extension to the random number
+
+let image4 = document.querySelectorAll("img")[3];   //selects the first image in red div
+
+image4.setAttribute("src", randomNumber4Source) // changes the source of the img based on number generated
